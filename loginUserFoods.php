aa2cad45123d8e7796558bcfb9126583505d38c6
Derrick <55024316+Bolarh@users.noreply.github.com>
2023-01-07 06:08:12 -0800

    Add files via upload

---
+++ loginUserFoods.php
@@ -0,0 +1,42 @@
+<?php  
+//session_start();
+include '../config/config.php';
+
+
+//$currentUser =$_SESSION['username'];
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+$sql ="SELECT * FROM users WHERE username ='$username' AND password ='$password'";
+
+$result = mysqli_query($conn, $sql);
+$count = mysqli_num_rows($result);
+
+if($count >0){    
+    $userRecord = array();
+    while($row = mysqli_fetch_assoc($result)){
+         $userRecord[] = $row;
+         
+    }
+    echo json_encode(
+    array(
+    "success"=> true,
+    "userData"=>$userRecord[0],
+    )
+    );
+    
+  
+}
+ else{
+    echo json_encode(
+    array(
+    "success"=> false
+    ));
+} 
+
+
+
+
+?>
